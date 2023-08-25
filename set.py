e7e7ace23cf6b2627b02d1a1b5c3fd8788c60527
Kimmy7070 <82769101+Kimmy7070@users.noreply.github.com>
2023-08-25 23:36:41 +0530

    test files

---
+++ set.py
@@ -0,0 +1,22 @@
+s={1,2,3}
+print(s,"",type(s))
+a={1,2,3}
+a.add(1)
+print(a)
+
+set1 = {"Yellow", "Orange", "Black"}
+set2 = {"Orange", "Blue", "Pink"}
+set3 = set2.difference(set1)
+print(set3)
+
+a={5,4}
+b={1,2,4,5}
+print(a<b)
+
+A = {30, 20, 10}
+B = {50, 30, 40}
+print(A.symmetric_difference(B))
+
+s={5,6}
+f=s
+print(f)
